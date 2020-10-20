b749c0287fd3bd799bc1291fba3536a26ba07122
Aurelien Tallet <aurelien.jarte@gmail.com>
2020-10-20 20:56:28 +0200

    update App.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,64 @@
 document.addEventListener('DOMContentLoaded', function () {
 	var app = {
-		nbPlayers: 3,
-		nbImp: 1,
-		nbWhite: 0,
+		amountPlayers: function (player) {
+			return game.nbPlayers = player
+		},
+	
 		init: function () {
 			app.helloworld;
 		},
 		helloworld: console.log("Impostor is running!")
 	}
+
+
+	const Player = function(name, victory = 0, alive = true) {
+		this.name = name;
+		this.victory = victory;
+		this.alive = alive
+	}
+
+	const Card = function(role, word, player = {}) {
+		this.role = role;
+		this.word = word;
+		this.player = player;
+		
+	}
+
 	var game = {
-		nbPlayers: app.nbPlayers,
+		board: document.querySelector('.board'),
+		nbPlayers: 3,
+		nbImpostor: 1,
+		nbWhite: 0,
+		playerList: [],
+		cardList:[],
 		init: function () {
-			app.helloworld;
+			game.getWords();
+			// game.fillBoard();
+			game.generateCards()
 		},
-		handleBtns: function () {
+		getWords: function () {
+			game.words = {goodWord: 'salut', badWord:'aurevoir'};
+		},
+		createPlayer: function (name, role, word) {
+			game.playerList.push(new Player(name, role, word));
+		},
+		fillBoard: function () {
+			for (let j = 0; j < game.nbPlayers; j++) {
+				game.board.innerHTML += `<div class='Card'>Joueur ${j}</div>`;
+			}
 
 		},
-		helloworld: console.log("Impostor is running!")
+		generateCards: function(){
+			let nbCitizens = game.nbImpostor + game.nbWhite
+			console.log(nbCitizens)
+			for (let i = 0; i < game.nbImpostor; i++) game.cardList.push(new Card(1,game.words.badWord))
+			for (let i = 0; i < game.nbWhite; i++) game.cardList.push(new Card(2,''))
+			for (let i = 0; i < game.nbPlayers - nbCitizens ; i++) game.cardList.push(new Card(0,game.words.goodWord))
+
+		}
+
 	}
 	app.init()
+	game.init()
+	console.log(game.cardList)
 })
